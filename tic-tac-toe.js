69a8868f9a3274b51d2d0b05ec4f3f1ffd984ede
Jelani-Plummer <119369634+Jelani-Plummer@users.noreply.github.com>
2023-10-22 14:22:37 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,22 +1,23 @@
-const squares = document.querySelectorAll('#board div');
-
 document.addEventListener('DOMContentLoaded', function () {
+    const squares = document.querySelectorAll('#board div');
+    const gameState = [];
     function initializeBoard() {
       squares.forEach(square => {
         square.classList.add('square');
         square.addEventListener('click', function(){
             if (!square.classList.contains('X') && !square.classList.contains('O')){
-                const player = gameState.length % 2 === 0 ? 'X' : 'O';
+                const player = gameState.length % 2 == 0 ? 'X' : 'O';
                 square.classList.add(player);
-                gameState[index] = player;
+                square.textContent = player;
+                gameState[gameState.length] = player;
             }
-
         })
       });
     }
   
     initializeBoard();
   });
+
 
 
 
